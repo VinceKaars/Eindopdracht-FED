728ba3fd04f684308811b0a0e5e006b22c725515
VinceKaars <vincekaars@131.145.028.145.hva.nl>
2016-11-04 10:42:46 +0100

    Bestanden voor FED

---
+++ frontend/script.js
@@ -0,0 +1,18 @@
+function toggleClass1(el){
+	if(el.className == "body"){
+		el.className = "bodydark";
+	} else {
+		el.className = "body";
+	}
+}
+
+
+function toggleClass(el){
+	if(el.className == "overlay"){
+		el.className = "overlayclick";
+	} else {
+		el.className = "overlay";
+	}
+}
+
+
